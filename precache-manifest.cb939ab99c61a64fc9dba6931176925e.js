c9b6f7d78c9486bd0c8fd1d684ea3b8be82402dd
LenaVeselova <lena.veselova88@gmail.com>
2020-04-08 21:37:37 +0300

    Updates

---
+++ precache-manifest.cb939ab99c61a64fc9dba6931176925e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1677a70d7d4ed3b36d06a0fb07958ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ba692f10bd865eca853",
+    "url": "/portfolio/static/css/main.19095c18.chunk.css"
+  },
+  {
+    "revision": "c136d0c6f1cf64707358",
+    "url": "/portfolio/static/js/2.c302f4ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c302f4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba692f10bd865eca853",
+    "url": "/portfolio/static/js/main.bc0a5ae9.chunk.js"
+  },
+  {
+    "revision": "e7c398ff0b2d375437da",
+    "url": "/portfolio/static/js/runtime-main.eeda1979.js"
+  }
+]);
